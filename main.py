5c75fdf0fdcc4f8a7e4117f34dd0f91ac6418ce3
Fahim-Nizamee <nizameef@gmail.com>
2023-11-18 17:54:21 +0600

    ggs

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-from flask import Flask, render_template, request, send_file,current_app
-from pytube import YouTube
-from pydub import AudioSegment
-from slugify import slugify
+from flask import Flask, render_template, request, send_file
 import os
+import urllib.parse
+import subprocess
 import secrets
 import shutil
-import urllib
-from concurrent.futures import ThreadPoolExecutor
-import time
 
 app = Flask(__name__)
-executor = ThreadPoolExecutor()
+
+def update_youtube_dl():
+    pass
 
 def download_video(video_url, download_directory):
-    yt = YouTube(video_url)
-    video = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first()
-    video_title = slugify(yt.title)
-    video.download(download_directory, filename=f'{video_title}.mp4')
-    return video_title
+    subprocess.run(['youtube-dl', '--format', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', '--extract-audio', '--audio-format', 'mp3', '-o', f'{download_directory}/%(title)s.%(ext)s', video_url])
 
 def convert_to_mp3(mp4_path, mp3_path):
-    audio = AudioSegment.from_file(mp4_path, format='mp4')
-    audio.export(mp3_path, format='mp3', bitrate='320k')
+    subprocess.run(['ffmpeg', '-i', mp4_path, '-vn', '-acodec', 'libmp3lame', '-b:a', '320k', mp3_path])
 
 @app.route('/')
 def index():
@@ -34,18 +27,23 @@ def convert():
 
     try:
         download_token = secrets.token_urlsafe(16)
-        download_directory = f'{current_app.root_path}/static/downloads/{download_token}'
+        download_directory = f'{app.root_path}/static/downloads/{download_token}'
         os.makedirs(download_directory)
         with open(f'{download_directory}/finished.txt', 'w'):
             pass
 
-        future = executor.submit(download_video, video_url, download_directory)
-        video_title = future.result()
+        # Update youtube-dl
+        update_youtube_dl()
 
-        mp4_path = f'{download_directory}/{video_title}.mp4'
-        mp3_path = f'{download_directory}/{video_title}.mp3'
-        executor.submit(convert_to_mp3, mp4_path, mp3_path)
-        time.sleep(2)
+        # Download the video
+        download_video(video_url, download_directory)
+
+        # Convert the video to MP3
+        video_title = os.listdir(download_directory)[0]  # Assumes only one video is downloaded
+        mp4_path = f'{download_directory}/{video_title}'
+        mp3_path = f'{download_directory}/{video_title.replace(".mp4", ".mp3")}'
+        convert_to_mp3(mp4_path, mp3_path)
+
         return render_template('success.html', download_link=f'/download/{download_token}/{video_title}')
 
     except Exception as e:
@@ -54,12 +52,11 @@ def convert():
 @app.route('/download/<token>/<filename>')
 def download(token, filename):
     decoded_filename = urllib.parse.unquote(filename)
-    download_path = f'{current_app.root_path}/static/downloads/{token}/{slugify(decoded_filename)}.mp3'
+    download_path = f'{app.root_path}/static/downloads/{token}/{decoded_filename}.mp3'
     return send_file(download_path, as_attachment=True)
-    
 
 def delete_folders_with_finished_txt():
-    base_directory = f'{current_app.root_path}/static/downloads/'
+    base_directory = f'{app.root_path}/static/downloads/'
     if os.path.exists(base_directory):
         for folder_name in os.listdir(base_directory):
             folder_path = os.path.join(base_directory, folder_name)
